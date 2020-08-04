39691c0044b824e8beab13e44f2c269e309aec72
harsh2912 <harshshrm94@gmail.com>
2020-08-04 21:10:33 +0000

    app.py

--- API/app.py
+++ API/app.py
@@ -8,12 +8,9 @@ import numpy as np
 import cv2
 import base64
 from PIL import Image
-from io import BytesIO
-from flask_cors import CORS
+import io 
 
 app = Flask(__name__)
-CORS(app)
-
 
 def get_array(arg):
     return np.array(arg).astype('uint8')
@@ -29,23 +26,16 @@ args = Matting_Args()
 matting_model = build_model(args)
 matting_model.eval();
 
-def get_response(new_bg,data,image):
+def get_response(new_bg,data):
+    image = get_array(data.get('image'))
     response = requests.post('http://127.0.0.1:3000/',json = data)
     if response.status_code == 406:
-        buffer = BytesIO()
-        Image.fromarray(image).save(buffer,format='png')
-        img_str = base64.b64encode(buffer.getvalue()).decode()
-#         print(img_str)
-        return {'output':img_str}
-    
+        return jsonify({'output':image.tolist()})
     h,w,_ = image.shape
     trimap = get_array(response.json()['trimap'])
     fg, bg, alpha = pred(image/255.0,trimap,matting_model)
     combined = ((alpha[...,None]*image)).astype('uint8') + ((1-alpha)[...,None]*cv2.resize(new_bg,(w,h))).astype('uint8')
-    buffer = BytesIO()
-    Image.fromarray(combined).save(buffer,format='png')
-    img_str = base64.b64encode(buffer.getvalue()).decode()
-    return {'output':img_str}
+    return jsonify({'output':combined.tolist()})
 
 @app.route('/with_bg',methods=['POST'])
 def extraction():
@@ -55,12 +45,9 @@ def extraction():
 
 @app.route('/',methods=["POST"])
 def extraction_without_bg():
-    data = request.files['file']
-    img = np.array(Image.open(data.stream).convert(mode='RGB'))
-#     print(img.shape)
-    data = {'image':img.tolist()}
+    data = request.get_json()
     new_bg = cv2.imread('1.jpg')[:,:,::-1]
-    return get_response(new_bg,data,img)
+    return get_response(new_bg,data)
         
     
 if __name__ == '__main__':
